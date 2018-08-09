6bc14cf7d4afa025abb914a9fb8cd4da4286e26c
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2018-08-09 19:56:02 -0400

    More work on trying to parse out arguments for the linear regression

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,14 @@
+import Foundation   //  Need this in order to use string methods etc.
+
 class MainDriver{
+
+    var xVals = [Double]()
+    var yVals = [Double]()
+
     func printUsage(){
         print("Simple Linear Regression in Swift")
-        print("-x\tRequired\tComma-separated list of X values")
-        print("-y\tRequired\tComma-separated list of y values")
+        print("x=12,13,...\tRequired\tComma-separated list of X values")
+        print("y=42.1,7...\tRequired\tComma-separated list of y values")
     }
 
 
@@ -14,11 +20,28 @@ var xVals = [String]()
 var yVals = [String]()
 for argument in CommandLine.arguments{
     print("arg=\(argument)")
+    if argument.hasPrefix("x="){
+        for item in (argument.split{$0 == ","}){
+            xVals.append(String(item))
+        }
+    }
+    else if argument.hasPrefix("y="){
+        for item in (argument.split{$0 == ","}){
+            yVals.append(String(item))
+        }
+    }
+
 }
+
+//  Now parse those arguments
+
 
 if xVals.isEmpty || yVals.isEmpty{
     mainDriver.printUsage()
 }
+
+print(xVals)
+print(yVals)
 
 // let arguments = C_ARGV
 // print(arguments)
---
+++ Tests/main/MainTest.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import main
+
+class MainTests: XCTestCase{
+    var sut : MainDriver!
+
+    override func setUp(){
+        
+    }
+
+    func testCheckSomething(){
+        XCTestAssertEqual(true, true)
+    }
+}
